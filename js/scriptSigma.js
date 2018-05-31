3e22d3c2add8676c90c455849b43785ad625064c
ninon <lizeninon@yahoo.fr>
2018-05-31 17:53:22 +0200

    version light du script

--- js/scriptSigma.js
+++ js/scriptSigma.js
@@ -1,7 +1,105 @@
-	var g = {
-        nodes: [],
-        edges: []
-    };
+var nodes =[
+    {
+      "id": "n0",
+      "label": "Jacques Derrida",
+      "x": 0,
+      "y": 0,
+      "size": 3,
+      "type": "goo"
+    },
+    {
+      "id": "n1",
+      "label": "Walter Benjamin",
+      "x": 3,
+      "y": 1,
+      "size": 2,
+      "type": "goo"
+    },
+    {
+      "id": "n2",
+      "label": "Emmanuel Kant",
+      "x": 1,
+      "y": 3,
+      "size": 0.5,
+      "type": "goo"
+    },
+    {
+      "id": "n3",
+      "label": "Aby Warburg",
+      "x": 10,
+      "y": 3,
+      "size": 1,
+      "type": "goo"
+    },
+
+    {
+      "id": "n4",
+      "label": "Ludwig Binswanger",
+      "x": 10,
+      "y": 10,
+      "size": 1,
+      "type": "goo"
+    },
+    {
+      "id": "n5",
+      "label": "Marcel Duchamp",
+      "x": 1,
+      "y": 10,
+      "size": 1,
+      "type": "goo"
+    },
+    {
+      "id": "n6",
+      "label": "Yves Klein",
+      "x": 5,
+      "y": 5,
+      "size": 1,
+      "type": "goo"
+    },
+    {
+      "id": "n7",
+      "label": "Nelson Goodman",
+      "x": 6,
+      "y": 8,
+      "size": 1,
+      "type": "goo"
+    },
+    {
+      "id": "n8",
+      "label": "Douglas Hoftstadter",
+      "x": 8,
+      "y": 2,
+      "size": 1,
+      "type": "goo"
+    }
+  ];
+  
+  var edges=[
+    {
+      "id": "e0",
+      "source": "n0",
+      "target": "n1"
+    },
+    {
+      "id": "e1",
+      "source": "n1",
+      "target": "n2"
+    },
+    {
+      "id": "e2",
+      "source": "n2",
+      "target": "n0"
+    }
+  ];
+
+
+
+// ici appel bdd tableau nodes et arcs : var g = graphData(nodes,edges);
+
+var g = {
+        nodes: nodes,
+        edges: edges
+    }; 
 
 
     // Visuel des noeuds
@@ -39,63 +137,13 @@
    settings: {
    	  minNodeSize: 5,
       maxNodeSize: 12,
-      // autoRescale: false,
-       mouseEnabled: true, // permet à la souris (mouseWheel) de zoomer dans le canvas
+       mouseEnabled: true, 
        mouseWheelEnabled: true,
-      // doubleClickEnabled: false,
-      // touchEnabled: false,
       nodesPowRatio: 1,
       edgesPowRatio: 1,
-      // defaultEdgeColor: 'white', 
       defaultNodeColor: '#A0A0A0',
       edgeColor: 'default'
     }
    });
 
-sigma.parsers.json('data/data_sigma.json', s ,  function() {
-    s.refresh();
-    console.log(s.graph.nodes());
-    s.startForceAtlas2();
 
-/*
-* Attention, le graphe est parsé de manière asynchrone, donc toute fonction
-*  devant précéder la création du graphe (forceAtlas2, ajout de nouveaux noeuds)
-*  doit se faire dans ce bloc 
-*
-*/
-
-document.querySelector("#newPost").addEventListener("click", 
-      function(event){
-        var layout=document.getElementById('d');
-        this.style.display='none';
-        var post=document.getElementById("myPost");
-        post.style.display="block";
-        document.getElementById("sent").style.display="block";
-        
-/*
-* Ajout d'un noeud au clic sur le bouton Envoyer
-* il reste à assurer que l'id soit unique, et faire le regroupement des noeuds par cluster de mots clés
-*
-*/
-        document.querySelector("#sent").addEventListener("click",
-          function(event){
-            this.style.display="none";
-            console.log("here!", post.value);
-            post.style.display="none";
-            document.getElementById("newPost").style.display='block';
-            s.graph.addNode({
-              id: 'n10',
-              label: post.value,
-              x: 1,
-              y: 1,
-              type: 'goo',
-              size: 1
-            });
-  post.value="";
-  console.log(s.graph.nodes());
-  s.refresh();
-  s.startForceAtlas2();
-
-          });
-      });
-  });
